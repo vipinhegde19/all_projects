326d502f3d656073f0e3a70f5a918a5710ac93d7
vipinhegde <vipinhegde19@gmail.com>
2024-07-17 11:11:39 +0530

    done completing encoder

--- python/decodeencode.py
+++ python/decodeencode.py
@@ -2,30 +2,40 @@ import random
 import string
 #encode
 def encode(word):
-    if len(word)>3:
+    if len(word)>=3:
         word=word[1:]+word[0]
         randomLetter = random.choice(string.ascii_letters)
         word=randomLetter+word+randomLetter
         return word
     else:
         word=word[::-1]
-        
+        return word
+
 #decode
-def decode(word):
-    if len(word)<3:
-        word=word[::-1]
+def decode(word,temp):
+    if len(temp)<3:
+        word=temp[::-1]
     else:
-        list=[]
+        list1=[]
         for i in word:
-            list.append(i)
-            del word[0:4]
-            del word[-1:-4]
-        word=word[1:]+word[-1]
+            list1.append(i)
+        del list1[0]
+        del list1[-1]
+        list1.insert(0,list1[-1])
+        list1.pop()
+        word=""
+        for i in list1:
+             word+=i
+    return word
         
-x=input("Enter the Number : ")
-y=int(input("Enter your choice 1.EC 2.DC"))
-match y:
-        case 1:
-                print(encode(x))
-        case 2:
-                print(decode(x))        
+word=input("Enter the string : ")
+temp=word
+while True:
+    y=int(input("Enter your choice 1.EC 2.DC: "))
+    if y==1:
+            word=encode(word)
+            print(word)
+    if y==2:
+            print(decode(word,temp))        
+    if y==3:
+            exit(0)
